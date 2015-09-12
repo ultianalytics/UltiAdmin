d2e9f3028c2c67068c0f4af8141458165d6b3571
Jim Geppert <jimgeppert@gmail.com>
2015-09-12 09:15:43 -0700

    added proxy-middleware to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,16 @@ var connect = require('gulp-connect');
 gulp.task('server', function(){
     connect.server({
         port: 9000,
-        livereload: true
+        livereload: true,
+        middleware: function(connect, o) {
+            return [ (function() {
+                var url = require('url');
+                var proxy = require('proxy-middleware');
+                var options = url.parse('http://www.ultianalytics.com/rest');
+                options.route = '/rest';
+                return proxy(options);
+            })() ];
+        }
     });
 });
 
