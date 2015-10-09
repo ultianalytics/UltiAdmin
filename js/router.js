38db2297c1ea2807049e1f6ccdb96bbde27b917a
Jim Geppert <jimgeppert@gmail.com>
2015-10-09 13:24:41 -0700

    Error dialog used by router

--- js/router.js
+++ js/router.js
@@ -1,48 +1,56 @@
 define(['jquery', 'underscore', 'backbone', 'appContext', 'collections/teams', 'collections/games', 'collections/players', 'views/AppView', 'appContext', 'bootbox'],
     function($, _, Backbone, appContext, teamCollection, gameCollection, playerCollection, AppView, appContext, bootbox) {
 
-    // NOTE: this returns an instance, not the constructor
+        // NOTE: this returns an instance, not the constructor
 
-    var AppRouter = Backbone.Router.extend({
-        appView: null,
-        routes: {
-            'team/:cloudId/:tab' : 'team',
-            '*path' : 'defaultRoute'
-        },
-        initialize: function() {
-            this.appView = new AppView();
-        },
-        defaultRoute: function(path) {
-            router = this;
-            if (appContext.hasCurrentUser()) {
-                teamCollection.ensureFetched(function () {
-                    if (!teamCollection.isEmpty()) {
-                        appContext.selectDefaultTeam();
-                        appContext.set('currentTab', 'settings');
+        var AppRouter = Backbone.Router.extend({
+            appView: null,
+            routes: {
+                'team/:cloudId/:tab' : 'team',
+                '*path' : 'defaultRoute'
+            },
+            initialize: function() {
+                this.appView = new AppView();
+            },
+            defaultRoute: function(path) {
+                var self = this;
+                if (appContext.hasCurrentUser()) {
+                    teamCollection.ensureFetched(function () {
+                        if (!teamCollection.isEmpty()) {
+                            appContext.selectDefaultTeam();
+                            appContext.set('currentTab', 'settings');
+                        }
+                        self.appView.render();
+                    }, function () {
+                        self.showServerErrorAlert();
+                    });
+                }
+            },
+
+            team: function(cloudId, tab) {
+                console.log('routed to cloud = ' + cloudId + ' tab = ' + tab);
+                var self = this;
+                teamCollection.ensureFetched(function() {
+                    var teamChange = appContext.currentTeamId() != cloudId;
+                    appContext.set('currentTeam', teamCollection.teamWithCloudId(cloudId));
+                    appContext.set('currentTab', tab == null ? 'settings' : tab);
+                    if (teamChange) {
+                        gameCollection.reset();
+                        playerCollection.reset();
                     }
-                    router.appView.render();
-                }, function () {
-                    alert("bad thang");
+                }, function() {
+                    self.showServerErrorAlert();
+                });
+            },
+            showServerErrorAlert: function () {
+                bootbox.alert({
+                    size: 'small',
+                    title: 'Server Error',
+                    message: 'Ouch...we experienced an error trying to talk to our server.<br/><br/>Please try again by refreshing your browser.<br/><br/>If the problem persists, please notify us at <a href="mailto:support@ultianalytics.com">support@ultianalytics.com</a>.'
                 });
             }
-        },
+        });
 
-        team: function(cloudId, tab) {
-            console.log('routed to cloud = ' + cloudId + ' tab = ' + tab);
-            teamCollection.ensureFetched(function() {
-                var teamChange = appContext.currentTeamId() != cloudId;
-                appContext.set('currentTeam', teamCollection.teamWithCloudId(cloudId));
-                appContext.set('currentTab', tab == null ? 'settings' : tab);
-                if (teamChange) {
-                    gameCollection.reset();
-                    playerCollection.reset();
-                }
-            }, function() {
-                alert("bad thang");
-            });
-        }
+        return new AppRouter();
+
     });
-
-    return new AppRouter();
-
-});
--- js/views/UltiView.js
+++ js/views/UltiView.js
@@ -2,7 +2,7 @@ define(['jquery', 'underscore', 'backbone', 'bootbox'], function($, _, Backbone,
 
     var UltiView = Backbone.View.extend({
         showServerErrorDialog: function () {
-            this.showErrorDialog('Server Error', 'Ouch...we experienced an error trying to talk to our server.<br/><br/>Please try again.  If the problem persists, please notify us');
+            this.showErrorDialog('Server Error', 'Ouch...we experienced an error trying to talk to our server.<br/><br/>Please try again by refreshing your browser.<br/><br/>If the problem persists, please notify us at <a href="mailto:support@ultianalytics.com">support@ultianalytics.com</a>.');
         },
         showErrorDialog: function (title, message) {
             bootbox.alert({
