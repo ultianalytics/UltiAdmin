971495a73fe9476fa5e0d1135464cf2597860386
Jim Geppert <jimgeppert@gmail.com>
2015-10-11 12:02:04 -0700

    Fixed "no teams" issue

--- js/views/TeamSelectorView.js
+++ js/views/TeamSelectorView.js
@@ -2,21 +2,22 @@ define(['jquery', 'underscore', 'backbone', 'collections/teams', 'appContext', '
     function($, _, Backbone, teamCollection, appContext, teamSelectorHtml) {
 
         var TeamSelectorView = Backbone.View.extend({
-            teams: teamCollection,
             el: '[ulti-team-selector]',
             initialize: function() {
-                this.teams.on('reset', this.teamsChanged, this);
+                teamCollection.on('reset', this.teamsChanged, this);
                 appContext.on("change:currentTeam", this.selectedTeamChanged, this);
             },
             template: _.template(teamSelectorHtml),
             teamsChanged: function() {
-                this.render();
+                if (!teamCollection.isEmpty()) {
+                    this.render();
+                }
             },
             selectedTeamChanged: function() {
                 this.render();
             },
             render: function() {
-                this.$el.html(this.template({teams : this.teams.models, selectedTeam : appContext.currentTeam()}));
+                this.$el.html(this.template({teams : teamCollection.models, selectedTeam : appContext.currentTeam()}));
                 this.$("[ulti-team-choice]").click(function(e) {
                     e.preventDefault();
                     var selectedCloudId = e.currentTarget.attributes['ulti-team-choice'].value;
